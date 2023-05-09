0b70b53dd66371dbe081e850fca5b7e2580c797d
mcarri355 <carrillomatisa@gmail.com>
2023-05-08 18:19:27 -0700

    portfolio intro
    I have almost finished the portfolio intro just need to fix some sizing issues as well as create the hack word rotation. Created the grid that will become my project intro.

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,35 @@
 function hamburger(x){
     x.classList.toggle("change");
 }
+
+// Hacker Animation For Word Rotation
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("span").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 60 );
+}
+
+$('greenBar')
