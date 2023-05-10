276e3ffb2e55d870a7788d180e9581298e1f5625
mcarri355 <carrillomatisa@gmail.com>
2023-05-09 18:21:50 -0700

    restart
    unsatisfied with my work, deleting everything going to restart with contact and about me

--- scripts/index.js
+++ scripts/index.js
@@ -3,34 +3,34 @@ function hamburger(x){
     x.classList.toggle("change");
 }
 
-// Hacker Animation For Word Rotation
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// // Hacker Animation For Word Rotation
+// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let interval = null;
+// let interval = null;
 
-document.querySelector("span").onmouseover = event => {  
-  let iteration = 0;
+// document.querySelector("span").onmouseover = event => {  
+//   let iteration = 0;
   
-  clearInterval(interval);
+//   clearInterval(interval);
   
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if(index < iteration) {
-          return event.target.dataset.value[index];
-        }
+//   interval = setInterval(() => {
+//     event.target.innerText = event.target.innerText
+//       .split("")
+//       .map((letter, index) => {
+//         if(index < iteration) {
+//           return event.target.dataset.value[index];
+//         }
       
-        return letters[Math.floor(Math.random() * 26)]
-      })
-      .join("");
+//         return letters[Math.floor(Math.random() * 26)]
+//       })
+//       .join("");
     
-    if(iteration >= event.target.dataset.value.length){ 
-      clearInterval(interval);
-    }
+//     if(iteration >= event.target.dataset.value.length){ 
+//       clearInterval(interval);
+//     }
     
-    iteration += 1 / 3;
-  }, 60 );
-}
+//     iteration += 1 / 3;
+//   }, 60 );
+// }
 
-$('greenBar')
+// $('greenBar')
